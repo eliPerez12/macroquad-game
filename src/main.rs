211e2ce5dfeb819f351116e521eac7dca30ad387
Eli Perez <pnaeli2006@gmail.com>
2023-08-13 12:00:15 -0700

    testing out preformance

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,8 @@ fn conf() -> Conf {
     }
 }
 
+// draw texture in grid
+
 fn draw_rect(rect: &Rect, color: Color) {
     draw_rectangle(rect.x, rect.y, rect.w, rect.h, color)
 }
@@ -20,35 +22,62 @@ fn default_camera_zoom() -> Vec2 {
 
 #[macroquad::main(conf)]
 async fn main() {
-    let rect1 = Rect {
-        x: 0.0,
-        y: 0.0,
-        w: 50.0,
-        h: 200.0,
-    };
-    let mut rotation: f32 = 0.0;
 
     let mut camera = Camera2D {
         zoom: default_camera_zoom(),
         ..Default::default()
     };
 
-    dbg!(screen_width(), screen_height());
+    let mut camera_zoom = 1.0;
+
+    let tiles_sheet = Texture2D::from_file_with_format(
+        include_bytes!("assets/jawbreaker_tiles.png"),
+        Some(ImageFormat::Png));
+    tiles_sheet.set_filter(FilterMode::Nearest);
 
     // Main game loop
     loop {
         clear_background(BLACK);
-        rotation += 0.8;
-        camera.zoom *= 1.002;
 
-        camera.rotation = rotation;
+        let camera_speed = 1.0;
+
+        if is_key_down(KeyCode::W) {
+            camera.target.y -= camera_speed;
+        }
+        if is_key_down(KeyCode::S) {
+            camera.target.y += camera_speed;
+        }
+        if is_key_down(KeyCode::A) {
+            camera.target.x -= camera_speed;
+        }
+        if is_key_down(KeyCode::D) {
+            camera.target.x += camera_speed;
+        }
+
+        if is_key_down(KeyCode::Q) {
+            camera_zoom *= 1.01;
+        }
+        if is_key_down(KeyCode::E) {
+            camera_zoom /= 1.01;
+        }
 
         // Draw in world space
         set_camera(&camera);
-        draw_rect(&rect1, RED);
-
+        camera.zoom = default_camera_zoom() * camera_zoom;
+        for y in 0..100 {
+            for x in 0..100 {
+                draw_texture_ex(&tiles_sheet, x as f32 * 8.0, y as f32* 8.0, WHITE, DrawTextureParams {
+                    dest_size: Some(Vec2::new(8.0, 8.0)),
+                    source: Some(Rect::new(7.0 * 8.0, 2.0 * 8.0, 8.0, 8.0)),
+                     ..Default::default()
+                    }
+                );
+            }
+        }
+        
         // Draw in screen space
         set_default_camera();
+        draw_text(&get_fps().to_string(), 50.0, 50.0, 100.0, WHITE);
 
         next_frame().await;
     }
